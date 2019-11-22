c64a13b7ad2e1881b69719248ac88eb6c834b248
Jan Ferko <julyloov@gmail.com>
2019-11-23 00:35:52 +0100

    Part 4 - React Hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Denny" animal="Dog" breed="Siberian Husky" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
