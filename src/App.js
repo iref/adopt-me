9a870b01c0f744fde8af9dc34e160104b38a7fd6
Jan Ferko <julyloov@gmail.com>
2019-11-22 02:28:42 +0100

    Part 3 - Use JSX for component markup

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Denny",
-      animal: "Dog",
-      breed: "Siberian Husky"
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Denny" animal="Dog" breed="Siberian Husky" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
